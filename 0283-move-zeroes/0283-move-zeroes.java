afb10033a20ffd144e227df9e6a5fc58e13c8bcf
Rijul Kumar Jain <102771948+rijulJain25@users.noreply.github.com>
2023-06-11 22:39:53 +0530

    Time: 1 ms (100%), Space: 44.9 MB (36.85%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,25 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int n = nums.length;
+        int arr[] = new int[n];
+        int j = 0;
+        for(int i = 0; i<n; i++){
+            if(nums[i] != 0){
+                arr[j] = nums[i];
+                j++;
+            }
+        }
+        for(int i = 0; i<n; i++){
+            if(nums[i] == 0){
+                arr[j] = nums[i];
+                j++;
+            }
+        }
+
+        for(int i = 0; i<n; i++){
+            nums[i] = arr[i];
+        }
+
+        
+    }
+}
