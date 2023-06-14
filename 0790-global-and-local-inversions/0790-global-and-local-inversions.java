a139149823c00acce05c94fd87596c8d26c7a1da
Rijul Kumar Jain <102771948+rijulJain25@users.noreply.github.com>
2023-06-14 20:55:15 +0530

    Time: 2 ms (69.95%), Space: 56.1 MB (54.86%) - LeetHub

--- 0790-global-and-local-inversions/0790-global-and-local-inversions.java
+++ 0790-global-and-local-inversions/0790-global-and-local-inversions.java
@@ -2,6 +2,7 @@ class Solution {
     public boolean isIdealPermutation(int[] A) {
         for (int i = 0; i < A.length; i++)
             if (i - A[i] > 1 || i - A[i] < -1) return false;
+        System.gc();
         return true;
     }
 }
