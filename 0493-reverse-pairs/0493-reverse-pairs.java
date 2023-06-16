f9fecb65816b767b76c1def4a36e54f8cce3a209
Rijul Kumar Jain <102771948+rijulJain25@users.noreply.github.com>
2023-06-16 21:10:17 +0530

    Time: 124 ms (42.07%), Space: 51 MB (62.19%) - LeetHub

--- 0493-reverse-pairs/0493-reverse-pairs.java
+++ 0493-reverse-pairs/0493-reverse-pairs.java
@@ -3,42 +3,51 @@ class Solution {
         return mergeSort(nums, 0, nums.length-1);
     }
 
-    int merge(int[] nums,int low,int mid, int high){
-        int count=0;
-        int j = mid+1;
-        for(int i=low;i<=mid;i++){
-            while(j<=high && nums[i]>(2*(long)nums[j])){
-                j++;
-            }count+=j-(mid+1);
+    public static int merge(int[] nums, int str, int mid, int high){
+        int right = mid+1;
+        int count = 0;
+        for(int i = str; i<=mid; i++){
+            while(right<= high && nums[i] > (2*(long)nums[right])){
+                right++;
+            }
+            count += (right -(mid+1));
         }
-        ArrayList<Integer>arr=new ArrayList<>();
-        int left=low,right=mid+1;
-        while(left<=mid && right<=high){
-            if(nums[left]<=nums[right]){
-                arr.add(nums[left++]);
+        int st1 = str;
+        int st2 = mid+1;
+        ArrayList<Integer> arr= new ArrayList<>();
+        while(st1<=mid && st2<= high){
+            if(nums[st1] > nums[st2]){
+                arr.add(nums[st2]);
+                st2++;
             }else{
-                arr.add(nums[right++]);
+                arr.add(nums[st1]);
+                st1++;
             }
         }
-        while(left<=mid){
-                arr.add(nums[left++]);
-            }
-        while(right<=high){
-                arr.add(nums[right++]);
-            }
-        for(int i=low;i<=high;i++){
-            nums[i]=arr.get(i-low);
+
+        while(st1<=mid){
+            arr.add(nums[st1]);
+            st1++;
+        }
+
+        while(st2<=high){
+            arr.add(nums[st2]);
+            st2++;
+        }
+        for(int i=str;i<=high;i++){
+            nums[i]=arr.get(i-str);
         }
         return count;
     }
-    public int mergeSort(int []nums,int low,int high){
-        if(low>=high){
+
+    public static int mergeSort(int[] nums, int str, int end){
+        if(str>=end){
             return 0;
         }
-        int mid=(low+high)/2;
-        int inv=mergeSort(nums,low,mid);
-        inv+=mergeSort(nums,mid+1,high);
-        inv+=merge(nums,low,mid,high);
+        int mid = (str+end)/2;
+        int inv = mergeSort(nums, str, mid);
+        inv += mergeSort(nums, mid+1, end);
+        inv += merge(nums, str, mid, end);
         return inv;
-    }
+}
 }
