c1f87d6467040857bd54921fea311e6f6e9deb1a
Rijul Kumar Jain <102771948+rijulJain25@users.noreply.github.com>
2023-06-24 19:23:36 +0530

    Time: 0 ms (100%), Space: 40.5 MB (91%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,20 +1,7 @@
 class Solution {
     public double myPow(double x, int n) {
-        double ans=1.0;
-        long nn=n;
-        if(nn<0) nn=-1*nn;
-        while(nn>0){
-            if(nn%2==1){
-                ans = ans *x;
-                nn=nn-1;
-            }
-            else{
-                x=x*x;
-                nn=nn/2;
-            }
-        }
-        if(n<0)
-        ans = (double)(1.0)/(double)(ans);
-        return ans;
+        if (n == 0) return 1;
+        if (n < 0) return 1 / x * myPow(1 / x, -(n +1));
+        return (n & 1) == 0 ? myPow(x * x, n / 2) : x * myPow(x * x, n / 2);
     }
 }
