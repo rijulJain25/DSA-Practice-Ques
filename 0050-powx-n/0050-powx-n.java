81568a1e92d0c5e648b9d7931aa7fba5d60ee649
Rijul Kumar Jain <102771948+rijulJain25@users.noreply.github.com>
2023-06-25 18:20:41 +0530

    Time: 0 ms (100%), Space: 40.7 MB (74.74%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,7 +1,13 @@
 class Solution {
     public double myPow(double x, int n) {
-        if (n == 0) return 1;
-        if (n < 0) return 1 / x * myPow(1 / x, -(n +1));
-        return (n & 1) == 0 ? myPow(x * x, n / 2) : x * myPow(x * x, n / 2);
+        if (n == 0) return 1.0;
+        // even power
+        if (n % 2 == 0)
+            return myPow(x * x, n / 2);
+        // odd power
+        if (n % 2 == 1)
+            return x * myPow(x, n - 1);        
+        // negative power
+        return 1/myPow(x, -n);
     }
 }
