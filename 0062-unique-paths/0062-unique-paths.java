577acb9777c9323eb3fa2c25424022b1ef7d14d2
Rijul Kumar Jain <102771948+rijulJain25@users.noreply.github.com>
2023-07-08 22:56:46 +0530

    Time: 0 ms (100%), Space: 39.6 MB (31.59%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,12 +1,24 @@
 class Solution {
 
-    public int uniquePaths(int m, int n) {
-        int N = m+n-2;
-        int r = m-1;
-        double res =1 ;
-        for(int i = 1; i<=r; i++){
-            res = res * (N-r+i)/i;
+    public int uniquePathsHelp(int dp[][], int right, int bottom){
+        if(right == 0 && bottom == 0){
+            return 1;
         }
-        return (int)res;
+        if(right < 0 || bottom < 0){
+            return 0;
+        }
+        if(dp[bottom][right] != -1){
+            return dp[bottom][right];
+        }
+        int up = uniquePathsHelp(dp, right, bottom-1);
+        int down = uniquePathsHelp(dp, right-1, bottom);
+        return dp[bottom][right] = up+down;
+    }
+
+    public int uniquePaths(int m, int n) {
+        int dp[][]= new int[m][n];
+        for (int[] row : dp)
+            Arrays.fill(row, -1);
+        return uniquePathsHelp(dp, n-1, m-1);
     }
 }
