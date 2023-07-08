a113288fe0a7b554f1edcdc8857f21e14d40a5c2
Rijul Kumar Jain <102771948+rijulJain25@users.noreply.github.com>
2023-07-08 23:25:14 +0530

    Time: 0 ms (100%), Space: 39.5 MB (44.94%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -16,27 +16,26 @@ class Solution {
     // }
 
     public int uniquePaths(int m, int n) {
-        int dp[][]= new int[m][n];
-        for (int[] row : dp)
-            Arrays.fill(row, -1);
-        
+        int dp[]= new int[n];
         for(int i = 0; i<m; i++){
+            int temp[]= new int[n];
             for(int j = 0; j<n; j++){
                 if(i == 0 && j == 0){
-                    dp[i][j] = 1;
+                    temp[j] = 1;
                     continue;
                 }
                 int up = 0;
                 int down = 0;
                 if(i>0){
-                    up = dp[i-1][j];
+                    up = dp[j];
                 }
                 if(j>0){
-                    down = dp[i][j-1];
+                    down = temp[j-1];
                 }
-                dp[i][j] = up+down;
+                temp[j] = up+down;
             }
+            dp = temp;
         }
-        return dp[m-1][n-1];
+        return dp[n-1];
     }
 }
