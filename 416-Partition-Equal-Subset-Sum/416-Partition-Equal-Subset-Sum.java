0cb7b0dd920b42193d195b0a47b844f17f7f10c0
Rijul Kumar Jain <102771948+rijulJain25@users.noreply.github.com>
2023-07-15 23:56:02 +0530

    Update 416-Partition-Equal-Subset-Sum.java

--- 416-Partition-Equal-Subset-Sum/416-Partition-Equal-Subset-Sum.java
+++ 416-Partition-Equal-Subset-Sum/416-Partition-Equal-Subset-Sum.java
@@ -18,4 +18,21 @@ class Solution {
         dp[ind][target]=notTaken||taken?1:0;
     return notTaken||taken;
 }
+    public boolean canPartition(int[] nums) {
+        int totSum=0;
+    
+    for(int i=0; i<nums.length;i++){
+        totSum+= nums[i];
+    }
+    
+    if (totSum%2==1) return false;
+    
+    else{
+        int k = totSum/2;
+        int dp[][]=new int[nums.length][k+1];
+        for(int row[]: dp)
+        Arrays.fill(row,-1);
+        return subsetSumUtil(nums.length-1,k,nums,dp);
+    } 
+    }
 }
