ddd04fe32ae301bb70512d984281c53365e80134
Rijul Kumar Jain <102771948+rijulJain25@users.noreply.github.com>
2023-07-25 23:52:22 +0530

    Time: 27 ms (8.54%), Space: 46.8 MB (81.13%) - LeetHub

---
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -0,0 +1,30 @@
+class Solution {
+
+    public static int prime = (int)(Math.pow(10, 9)+7);
+    public int numDistinct(String s, String t) {
+        int n = s.length();
+        int m = t.length();
+
+        int[][] dp = new int[n+1][m+1];
+
+        for(int i = 0; i<= n; i++){
+            dp[i][0] = 1;
+        }
+
+        for(int i = 1; i<= m; i++){
+            dp[0][i] = 0;
+        }
+
+        for(int i = 1; i<=n; i++){
+            for(int j = 1; j<=m; j++){
+                if(s.charAt(i-1) == t.charAt(j-1)){
+                    dp[i][j] = (dp[i-1][j-1]+ dp[i-1][j])%prime;
+                }else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][m];
+
+    }
+}
