bbc0d3ad328791188e5068ea0bbec84f5dad8bbf
Rijul Kumar Jain <102771948+rijulJain25@users.noreply.github.com>
2023-07-25 22:36:59 +0530

    Time: 16 ms (95.2%), Space: 44.9 MB (57.47%) - LeetHub

--- 1170-shortest-common-supersequence/1170-shortest-common-supersequence.java
+++ 1170-shortest-common-supersequence/1170-shortest-common-supersequence.java
@@ -1,62 +1,41 @@
 class Solution {
-    public String shortestCommonSupersequence(String str1, String str2) {
-        
-        int n = str1.length();
-        int m = str2.length();
-
-        int dp[][] = new int[n+1][m+1];
-
-        for(int[] row: dp){
-            Arrays.fill(row, -1);
+    public String shortestCommonSupersequence(String s1, String s2) {
+    // find the LCS of s1 and s2
+    int m = s1.length(),n=s2.length();
+    int dp[][] = new int[m+1][n+1];
+    // fill the dp array using dynamic programming
+    for(int i=1;i<=m;i++){
+        for(int j=1;j<=n;j++){
+            if(s1.charAt(i-1)==s2.charAt(j-1)){
+                dp[i][j]=1+dp[i-1][j-1];
+            }else dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
         }
-
-        for(int i = 0; i<=n; i++){
-            dp[i][0] = 0;
-        }
-
-        for(int j = 0; j<=m; j++){
-            dp[0][j] = 0;
-        }
-
-        for(int i = 1; i<=n; i++){
-            for(int j = 1; j<= m; j++){
-                if(str1.charAt(i-1) == str2.charAt(j-1)){
-                    dp[i][j] = 1+dp[i-1][j-1];
-                }else{
-                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
-                }
-            }
-        }
-
-        int len = dp[n][m];
-        int i = n;
-        String str = "";
-        int j = m;
-        while(i>0 && j>0){
-            if(str1.charAt(i-1) == str2.charAt(j-1)){
-                str += str1.charAt(i-1);
-                i--;
-                j--;
-            }else if(dp[i-1][j] > dp[i][j-1]){
-                str += str1.charAt(i-1);
-                i--;
-            }else{
-                str += str2.charAt(j-1);
-                j--;
-            }
-        }
-
-        while(i>0){
-            str += str1.charAt(i-1);
-            i--;
-        }
-
-         while(j>0){
-            str += str2.charAt(j-1);
-            j--;
-        }
-
-        String ans = new StringBuilder(str).reverse().toString();
-        return ans;
     }
+    // backtrack from the bottom right corner of the dp array to find the LCS
+    StringBuilder sb = new StringBuilder();
+    int i=m,j=n;
+    while(i>0 && j>0){
+        if(s1.charAt(i-1)==s2.charAt(j-1)){
+            sb.append(s1.charAt(i-1));
+            i--;
+            j--;
+        }else if(dp[i-1][j]>dp[i][j-1]){//shrink s1
+        sb.append(s1.charAt(i-1));
+         i--;
+        }else{
+            sb.append(s2.charAt(j-1));
+         j--;
+        }
+    }
+    while(i>0){//remaing 1 element in s1
+    sb.append(s1.charAt(i-1));
+         i--;
+    }
+     while(j>0){
+    sb.append(s2.charAt(j-1));
+         j--;
+    }
+    // reverse the LCS string and return it
+    return sb.reverse().toString();
+}
 }
