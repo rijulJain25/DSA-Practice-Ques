af18f7fb38e3be2b7d4a04e41e359d6b0e313a0e
Rijul Kumar Jain <102771948+rijulJain25@users.noreply.github.com>
2023-07-28 01:40:19 +0530

    Time: 1 ms (100%), Space: 53 MB (57.78%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,16 +1,31 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int count = 0; 
-        int prod = 1;
-        for(int i = 0; i<nums.length; i++){
-            count += nums[i] == 0 ? 1: 0;
-            prod *= nums[i] == 0 ? 1: nums[i];
-        }
+        // int count = 0; 
+        // int prod = 1;
+        // for(int i = 0; i<nums.length; i++){
+        //     count += nums[i] == 0 ? 1: 0;
+        //     prod *= nums[i] == 0 ? 1: nums[i];
+        // }
 
-        for(int i = 0; i<nums.length; i++){
-            nums[i] = nums[i] == 0 ? (count> 1? 0 : (int)prod) : (count > 0 ? 0 : (int)(prod/nums[i]));
-        }
-        System.gc();
-        return nums;
+        // for(int i = 0; i<nums.length; i++){
+        //     nums[i] = nums[i] == 0 ? (count> 1? 0 : (int)prod) : (count > 0 ? 0 : (int)(prod/nums[i]));
+        // }
+        // System.gc();
+        // return nums;
+
+        int right = 1;
+        int left = 1;
+        int op[] = new int[nums.length];
+
+        for(int i = nums.length-1; i>= 0; i--){
+            op[i] = right;
+            right *= nums[i];
+        } 
+
+        for(int j = 0; j<nums.length; j++){
+            op[j] *= left;
+            left *= nums[j];
+        } 
+        return op;
     }
 }
