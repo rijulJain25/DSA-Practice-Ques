64a684a3e023941d1299f277ef6747a6062fb613
Rijul Kumar Jain <102771948+rijulJain25@users.noreply.github.com>
2023-07-28 00:49:18 +0530

    Time: 64 ms (44.29%), Space: 66.3 MB (25.67%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -9,18 +9,15 @@ class Solution {
             }
         }
 
-        int profit = 0;
-
         for(int i = prices.length-1; i>= 0; i--){
             for(int buy = 0; buy<=1; buy++){
                 for(int cap = 1; cap<=2; cap++){
                     if(buy == 0){
-                        profit = Math.max(dp[i+1][1][cap] - prices[i], dp[i+1][0][cap]);
+                        dp[i][buy][cap] = Math.max(dp[i+1][1][cap] - prices[i], dp[i+1][0][cap]);
                     }else{
-                        profit = Math.max(dp[i+1][0][cap-1] + prices[i], dp[i+1][1][cap]);
+                        dp[i][buy][cap] = Math.max(dp[i+1][0][cap-1] + prices[i], dp[i+1][1][cap]);
                     }
 
-                    dp[i][buy][cap] = profit;
                 }
             }
         }
