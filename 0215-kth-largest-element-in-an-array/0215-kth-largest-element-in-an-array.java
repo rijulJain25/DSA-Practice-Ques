49b4524366f3766ea121e7f3f2c8ee21fb0ce74f
Rijul Kumar Jain <102771948+rijulJain25@users.noreply.github.com>
2023-07-28 00:24:27 +0530

    Time: 2 ms (100%), Space: 56.4 MB (25.14%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,9 +1,29 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
 
-        Arrays.sort(nums);
-        System.gc();
-        return nums[nums.length-k];
+       int min = 0;
+        int max = 0;
+        for (var num : nums) {
+            if (num > max) {
+                max = num;
+            } else if (num < min) {
+                min = num;
+            }
+        }
+        
+        int[] num2frequency = new int[max - min + 1];
+        for (var num : nums) {
+            num2frequency[num - min]++;
+        }
+
+        for (var i = num2frequency.length-1; i >= 0; i--) {
+            var frequency = num2frequency[i];
+            k -= frequency;
+            if (k <= 0) {
+                return i + min;
+            }
+        }
+        throw new IllegalStateException();
         
     }
 }
