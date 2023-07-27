7b911474f835d1685640f66c4919330f53380838
Rijul Kumar Jain <102771948+rijulJain25@users.noreply.github.com>
2023-07-27 23:39:03 +0530

    Time: 1 ms (89.49%), Space: 44.2 MB (79.33%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,30 +1,46 @@
 class Solution {
     
-    public static int helper(int[] price, int ind, int buy, int[][] dp){
-        if(ind == price.length){
-            return 0;
-        }
+    // public static int helper(int[] price, int ind, int buy, int[][] dp){
+    //     if(ind == price.length){
+    //         return 0;
+    //     }
 
-        if(dp[ind][buy] != -1){
-            return dp[ind][buy];
-        }
+    //     if(dp[ind][buy] != -1){
+    //         return dp[ind][buy];
+    //     }
 
-        int profit;
+    //     int profit;
 
-        if(buy == 0){
-            profit =  Math.max(helper(price, ind+1, 1, dp)-price[ind], helper(price, ind+1, 0, dp));
-        }else{
-            profit =  Math.max(price[ind]+helper(price, ind+1, 0, dp), helper(price, ind+1, 1, dp));
-        }
-        return dp[ind][buy] = profit;
-    }
+    //     if(buy == 0){
+    //         profit =  Math.max(helper(price, ind+1, 1, dp)-price[ind], helper(price, ind+1, 0, dp));
+    //     }else{
+    //         profit =  Math.max(price[ind]+helper(price, ind+1, 0, dp), helper(price, ind+1, 1, dp));
+    //     }
+    //     return dp[ind][buy] = profit;
+    // }
 
     public int maxProfit(int[] prices) {
-        int dp[][] = new int[prices.length][2];
+        int dp[][] = new int[prices.length+1][2];
         for(int row[]: dp){
             Arrays.fill(row, -1);
         }
 
-        return helper(prices, 0, 0, dp);
+        dp[prices.length][0] = 0;
+        dp[prices.length][1] = 0;
+
+        int profit;
+
+        for(int ind = prices.length-1; ind>=0; ind--){
+            for(int buy = 0; buy<=1; buy++){
+                if(buy == 0){
+                    profit =  Math.max(dp[ind+1][1]-prices[ind], dp[ind+1][0]);
+                }else{
+                    profit =  Math.max(prices[ind]+dp[ind+1][0], dp[ind+1][1]);
+                }
+                dp[ind][buy] = profit;
+            }
+        }
+
+        return dp[0][0];
     }
 }
