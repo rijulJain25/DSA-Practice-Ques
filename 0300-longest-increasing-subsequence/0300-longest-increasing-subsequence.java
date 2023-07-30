71d043062678f742f2cb853bd67ad5140dfa5ac9
Rijul Kumar Jain <102771948+rijulJain25@users.noreply.github.com>
2023-07-30 14:17:32 +0530

    Time: 108 ms (24.61%), Space: 94.8 MB (17.73%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,28 +1,40 @@
 class Solution {
 
-    public static int helper(int nums[], int str, int prev, int[][] dp){
-        if(str == nums.length){
-            return 0;
-        }
-        if(dp[str][prev+1] != -1){
-            return dp[str][prev+1];
-        }
+    // public static int helper(int nums[], int str, int prev, int[][] dp){
+    //     if(str == nums.length){
+    //         return 0;
+    //     }
+    //     if(dp[str][prev+1] != -1){
+    //         return dp[str][prev+1];
+    //     }
 
-        int not = helper(nums, str+1, prev, dp);
-        int take = 0;
-        if(prev == -1 || nums[str] > nums[prev]){
-            take = 1+ helper(nums, str+1, str, dp);
-        }
-        return dp[str][prev+1] = Math.max(not, take);
-    }
+    //     int not = helper(nums, str+1, prev, dp);
+    //     int take = 0;
+    //     if(prev == -1 || nums[str] > nums[prev]){
+    //         take = 1+ helper(nums, str+1, str, dp);
+    //     }
+    //     return dp[str][prev+1] = Math.max(not, take);
+    // }
 
     public int lengthOfLIS(int[] nums) {
-        int dp[][] = new int[nums.length][nums.length+1];
+        int dp[][] = new int[nums.length+1][nums.length+1];
 
-        for(int row[] : dp){
-            Arrays.fill(row, -1);
+        for(int row[]: dp){
+            Arrays.fill(row, 0);
         }
 
-        return helper(nums, 0, -1, dp);
+        for(int i = nums.length-1; i>= 0; i--){
+            for(int prev = i-1; prev>= -1; prev--){
+                int not = dp[i+1][prev+1];
+                int take = 0;
+                if(prev == -1 || nums[i] > nums[prev]){
+                    take = 1+ dp[i+1][i+1];
+                }
+
+                dp[i][prev+1] = Math.max(take, not);
+            }
+        }
+
+        return dp[0][0];
     }
 }
