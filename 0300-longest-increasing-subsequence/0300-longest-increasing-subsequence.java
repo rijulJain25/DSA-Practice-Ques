5be63608823ca18200645c49517c6bc90c7fc729
Rijul Kumar Jain <102771948+rijulJain25@users.noreply.github.com>
2023-07-30 22:44:37 +0530

    Time: 5 ms (89.85%), Space: 43 MB (91.72%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 class Solution {
 
     // public static int helper(int nums[], int str, int prev, int[][] dp){
@@ -17,24 +19,40 @@ class Solution {
     // }
 
     public int lengthOfLIS(int[] nums) {
-        int dp[][] = new int[nums.length+1][nums.length+1];
+        // int dp[] = new int[nums.length+1];
+        // int curr[] = new int[nums.length+1];
 
-        for(int row[]: dp){
-            Arrays.fill(row, 0);
-        }
+        // for(int i = nums.length-1; i>= 0; i--){
+        //     for(int prev = i-1; prev>= -1; prev--){
+        //         int not = dp[prev+1];
+        //         int take = 0;
+        //         if(prev == -1 || nums[i] > nums[prev]){
+        //             take = 1+ dp[i+1];
+        //         }
 
-        for(int i = nums.length-1; i>= 0; i--){
-            for(int prev = i-1; prev>= -1; prev--){
-                int not = dp[i+1][prev+1];
-                int take = 0;
-                if(prev == -1 || nums[i] > nums[prev]){
-                    take = 1+ dp[i+1][i+1];
-                }
+        //         curr[prev+1] = Math.max(take, not);
+        //     }
+        //     dp = curr;
+        // }
 
-                dp[i][prev+1] = Math.max(take, not);
+        // return curr[0];
+
+        List<Integer> sub = new ArrayList<>();
+
+        for (int num : nums) {
+            int i = Collections.binarySearch(sub, num);
+
+            if (i < 0) {
+                i = -(i + 1);
+            }
+
+            if (i == sub.size()) {
+                sub.add(num);
+            } else {
+                sub.set(i, num);
             }
         }
-
-        return dp[0][0];
+        
+        return sub.size();
     }
 }
