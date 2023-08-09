32f988646de2641343cfd28093d0f1c8e914f2cb
Rijul Kumar Jain <102771948+rijulJain25@users.noreply.github.com>
2023-08-10 00:12:15 +0530

    Time: 0 ms (100%), Space: 38.7 MB (99.48%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,15 @@
+class Solution {
+
+    // public static int helper(int n){}
+
+    public int climbStairs(int n) {
+        int prev1= 1;
+        int prev2 = 1;
+        for(int i = 2; i<=n; i++){
+            int curr = prev1+prev2;
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return prev1;
+    }
+}
