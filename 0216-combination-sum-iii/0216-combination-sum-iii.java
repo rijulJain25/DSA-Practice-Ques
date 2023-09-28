90b448a7b5b3977607c0991fa8f7ed1ba689916e
Rijul Kumar Jain <102771948+rijulJain25@users.noreply.github.com>
2023-09-28 20:06:08 +0530

    Time: 0 ms (100%), Space: 39.6 MB (98.09%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.java
+++ 0216-combination-sum-iii/0216-combination-sum-iii.java
@@ -1,24 +1,21 @@
 class Solution {
 
-    public static void Helper(int str, int n, int k , int end, List<List<Integer>> res, List<Integer> list){
-        // if(res.contains(list)){
-        //     return;
-        // }
-        if(n == 0 && list.size() == k){
-            res.add(new ArrayList<>(list));
+    public static void helper(int k, int n , int st, List<Integer> list, List<List<Integer>> val){
+
+        if(list.size()== k && n == 0){
+            val.add(new ArrayList<>(list));
         }
-        
-       for(int i = str; i<=9; i++){
-           list.add(i);
-           Helper(i+1, n-i, k, end, res, list);
-           list.remove(list.size()-1);
-       }
+
+        for(int i= st; i<=9; i++){
+            list.add(i);
+            helper(k, n-i, i+1, list, val);
+            list.remove(list.size()-1);
+        }
     }
-
+    
     public List<List<Integer>> combinationSum3(int k, int n) {
-        List<List<Integer>> res= new ArrayList<>();
-        Helper(1, n, k, 9, res, new ArrayList<>());
-        return res;
-
+        List<List<Integer>> ans = new ArrayList<>();
+        helper(k, n, 1, new ArrayList<Integer>(), ans);
+        return ans;
     }
 }
