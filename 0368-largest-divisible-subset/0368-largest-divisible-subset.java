353bd8175b60eab6ca7b4be86bac7cc788796656
Rijul Kumar Jain <102771948+rijulJain25@users.noreply.github.com>
2023-10-10 21:20:47 +0530

    Time: 15 ms (32.93%), Space: 42.3 MB (52.02%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.java
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.java
@@ -1,41 +1,42 @@
-import java.util.*;
-
 class Solution {
-
-    public static List<Integer> largestDivisibleSubset(int[] nums) {
-
-        int n = nums.length;
+    public List<Integer> largestDivisibleSubset(int[] nums) {
         Arrays.sort(nums);
+        int n = nums.length;
 
-        int dp[] = new int[n];
+        int[] dp = new int[nums.length];
+        int[] hash = new int[nums.length];
         Arrays.fill(dp, 1);
-        int hash[] = new int[n];
-        Arrays.fill(hash, 1);
-        for(int i = 0; i< n; i++){
+        Arrays.fill(hash, 0);
+
+        for(int i = 0; i<n ;i++){
             hash[i] = i;
-            for(int prev = 0; prev<=i-1; prev++){
-                if(nums[i]%nums[prev] == 0 && dp[i]<dp[prev]+1){
-                    dp[i] = dp[prev]+1;
-                     hash[i] = prev;
+            for(int prev = 0; prev <=i-1; prev++){
+                if(nums[i]%nums[prev] == 0 && 1+dp[prev]> dp[i]){
+                    dp[i] = 1+dp[prev];
+                    hash[i] = prev;
                 }
             }
         }
+
         int ans = -1;
         int lstIdx = -1;
         for(int i = 0; i<n; i++){
-            if(dp[i]>ans){
+            if(dp[i] > ans){
                 ans = dp[i];
                 lstIdx = i;
             }
         }
 
-        List<Integer> list= new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
         list.add(nums[lstIdx]);
-        while(hash[lstIdx]!=lstIdx){
+
+        while(hash[lstIdx] != lstIdx){
             lstIdx = hash[lstIdx];
             list.add(nums[lstIdx]);
         }
+
         Collections.reverse(list);
         return list;
+
     }
 }
