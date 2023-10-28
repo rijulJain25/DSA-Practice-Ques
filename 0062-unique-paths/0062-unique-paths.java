104cf1e0e3508c6b8d941cdac257dba52dd7c64f
Rijul Kumar Jain <102771948+rijulJain25@users.noreply.github.com>
2023-10-28 19:09:09 +0530

    Time: 0 ms (100%), Space: 38.8 MB (96.16%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,41 +1,23 @@
 class Solution {
 
-    // public int uniquePathsHelp(int dp[][], int right, int bottom){
-    //     if(right == 0 && bottom == 0){
-    //         return 1;
-    //     }
-    //     if(right < 0 || bottom < 0){
-    //         return 0;
-    //     }
-    //     if(dp[bottom][right] != -1){
-    //         return dp[bottom][right];
-    //     }
-    //     int up = uniquePathsHelp(dp, right, bottom-1);
-    //     int down = uniquePathsHelp(dp, right-1, bottom);
-    //     return dp[bottom][right] = up+down;
-    // }
-
-    public int uniquePaths(int m, int n) {
-        int dp[]= new int[n];
-        for(int i = 0; i<m; i++){
-            int temp[]= new int[n];
-            for(int j = 0; j<n; j++){
-                if(i == 0 && j == 0){
-                    temp[j] = 1;
-                    continue;
-                }
-                int up = 0;
-                int down = 0;
-                if(i>0){
-                    up = dp[j];
-                }
-                if(j>0){
-                    down = temp[j-1];
-                }
-                temp[j] = up+down;
-            }
-            dp = temp;
+    public static int helper(int i, int j, int[][] dp){
+        if(i == 0 && j == 0){
+            return 1;
         }
-        return dp[n-1];
+        if(i<0 || j<0){
+            return 0;
+        }
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        dp[i][j] = helper(i-1, j, dp)+helper(i, j-1, dp);
+        return dp[i][j];
+    }
+    public int uniquePaths(int m, int n) {
+        int dp[][] = new int[m][n];
+        for(int rows[]: dp){
+            Arrays.fill(rows, -1);
+        }
+        return helper(m-1, n-1, dp);
     }
 }
