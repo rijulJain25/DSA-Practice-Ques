2811893b738ac7da97d1fd334a8fb8fa75a0d887
Rijul Kumar Jain <102771948+rijulJain25@users.noreply.github.com>
2023-10-28 19:21:14 +0530

    Time: 0 ms (100%), Space: 40.5 MB (61.38%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,53 +1,30 @@
 class Solution {
-    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        if(obstacleGrid==null||obstacleGrid.length==0)
+    public static int helper(int arr[][], int i, int j, int[][] dp){
+        if(i == 0 && j == 0){
+            return 1;
+        }
+        if(i>=0 && j>=0 && arr[i][j] == 1){
             return 0;
-        int m = obstacleGrid.length;
-        int n = obstacleGrid[0].length;
-        if(obstacleGrid[0][0]==1||obstacleGrid[m-1][n-1]==1)
+        }
+        if(i<0 || j<0){
             return 0;
-        int[][] dp = new int[m][n];
-        dp[0][0]=1;
-        //left column
-        for(int i=1; i<m; i++)
-        {
-            if(obstacleGrid[i][0]==1)
-            {
-                dp[i][0] = 0;
-            }
-            else
-            {
-                dp[i][0] = dp[i-1][0];
-            }
         }
-        //top row
-        for(int i=1; i<n; i++)
-        {
-            if(obstacleGrid[0][i]==1)
-            {
-                dp[0][i] = 0;
-            }
-            else
-            {
-                dp[0][i] = dp[0][i-1];
-            }
+        if(dp[i][j] != -1){
+            return dp[i][j];
         }
-        //fill up cells inside
-        for(int i=1; i<m; i++)
-        {
-            for(int j=1; j<n; j++)
-            {
-                if(obstacleGrid[i][j]==1)
-                {
-                    dp[i][j]=0;
-                }
-                else
-                {
-                    dp[i][j]=dp[i-1][j]+dp[i][j-1];
-                }
-            }
-        }
-        return dp[m-1][n-1];
-    
+        dp[i][j] = helper(arr, i-1, j, dp)+helper(arr, i, j-1, dp);
+        return dp[i][j];
     }
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length; int n = obstacleGrid[0].length;
+        int dp[][] = new int[m][n];
+        if(obstacleGrid[0][0] == 1){
+            return 0;
+        }
+        for(int rows[]: dp){
+            Arrays.fill(rows, -1);
+        }
+        return helper(obstacleGrid, m-1, n-1, dp);
+    }
+
 }
